cb5068261144c442ba1762aa95439a2ad5383ec6
Gleydson Epifanio <gles@outlook.com.br>
2024-05-25 18:42:10 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.51.1"
     }
   }
+  backend "s3" {
+    bucket = "tfstate-654654371202"
+    key    = "lab-gleydson-eks/terraform.tf.state"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1"
 }
